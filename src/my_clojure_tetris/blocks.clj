91ba7b06a81690692d816c8abce138bdaaa500c4
Evandro Souza <evandroferreiras@gmail.com>
2021-05-22 14:25:29 -0300

    refactor: recolored tetris pieces

--- src/my_clojure_tetris/blocks.clj
+++ src/my_clojure_tetris/blocks.clj
@@ -2,15 +2,20 @@
   (:require [my-clojure-tetris.schemas :as schemas]
             [schema.core :as s]))
 
+(def dark-orange {:r 255 :g 140 :b 0})
+(def lime-green {:r 0 :g 150 :b 0})
+(def crimson {:r 220 :g 20 :b 60})
+(def dark-turquoise {:r 0 :g 206 :b 209})
+(def yellow {:r 254 :g 226 :b 62})
+
 (s/defn get-color :- schemas/Color
   [block :- (s/maybe schemas/Block)]
   (let [type (:type block)]
     (cond
-      (= type :STRAIGHT) {:r 0 :g 75 :b 255}
-      (= type :L) {:r 255 :g 17 :b 0}
-      (= type :SQUARE) {:r 254 :g 226 :b 62}
-      (= type :T) {:r 99 :g 200 :b 62}
-      (= type :SKEW) {:r 219 :g 48 :b 130}
-      (= type :INVERTED-SKEW) {:r 27 :g 161 :b 266}
+      (= type :STRAIGHT) dark-turquoise
+      (= type :L) dark-orange
+      (= type :SQUARE) yellow
+      (= type :SKEW) lime-green
+      (= type :INVERTED-SKEW) crimson
       (= type :INVERTED-L) {:r 255 :g 129 :b 0}
-      :else {:r 40 :g 40 :b 40})))
+      :else {:r 60 :g 60 :b 60})))
